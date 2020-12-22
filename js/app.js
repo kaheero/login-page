cd0a8bc2593fc2400e312e5655a47563a00d68e6
Kairo Rodrigues dos Santos <kairo.santos@gmail.com>
2020-12-22 12:44:50 -0300

    front-end login screen with social network: google, facebook, linkedin and github

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+var btnSignIn = document.querySelector("#signin");
+var btnSignUp = document.querySelector("#signup");
+
+var body = document.querySelector("body");
+
+btnSignIn.addEventListener("click", function(){
+    body.className = "sign-in-js";
+});
+
+btnSignUp.addEventListener("click", function(){
+    body.className = "sign-up-js";
+});
